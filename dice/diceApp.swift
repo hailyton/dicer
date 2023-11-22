b3964a5a3e373ba34772e32f6824f401892a24e1
hailyton <hailyton00@gmail.com>
2023-11-22 22:41:43 +0700

    Initial Commit

---
+++ dice/diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceApp.swift
+//  dice
+//
+//  Created by halley on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
